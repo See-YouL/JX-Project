6432bfef33223009cde641df524cfe945d8150be
See-YouL <jff517680@gmail.com>
2025-01-06 08:45:18 +0800

    完成GPIO输出, LED闪烁实验

--- GPIO_OUT_LED-Flashing/Doc/README.md
+++ GPIO_OUT_LED-Flashing/Doc/README.md
@@ -291,3 +291,39 @@ void GPIO_Write(GPIO_TypeDef* GPIOx, uint16_t PortVal)
 ### 程序设计
 
 实现红灯闪烁, PB5低电平点亮
+
+GPIO端口设置
+
+1. 使能GPIOB时钟
+2. 配置PB5为推挽输出, 50MHz
+
+```c
+// 1. 使能GPIOB时钟
+RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+
+// 2. 配置PB5为推挽输出, 50MHz
+GPIO_InitTypeDef GPIO_InitStructure; // 定义GPIO初始化结构体
+GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5; // 端口5
+GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; // 推挽输出
+GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; // 50MHz
+GPIO_Init(GPIOB, &GPIO_InitStructure); // 初始化GPIOB
+```
+
+LED控制
+
+1. 点亮LED
+2. 延时
+3. 熄灭LED
+4. 延时
+
+延时函数为提供代码, 非自写
+
+```c
+while(1)
+{
+    GPIO_ResetBits(GPIOB, GPIO_Pin_5); // PB5输出低电平, 点亮红灯
+    Delay_ms(500); // 延时500ms
+    GPIO_SetBits(GPIOB, GPIO_Pin_5); // PB5输出高电平, 熄灭红灯
+    Delay_ms(500); // 延时500ms
+}
+```
--- GPIO_OUT_LED-Flashing/User/main.c
+++ GPIO_OUT_LED-Flashing/User/main.c
@@ -14,6 +14,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
+#include "Delay.h"
 
 
 /**
@@ -23,6 +24,23 @@
  */
 int main(void)
 {
+    // 使能GPIOB时钟
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+
+    // GPIO Configuration
+    GPIO_InitTypeDef GPIO_InitStructure; // 定义GPIO初始化结构体
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5; // 端口5
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; // 推挽输出
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz; // 50MHz
+    GPIO_Init(GPIOB, &GPIO_InitStructure); // 初始化GPIOB
+
+
+    while(1)
+    {
+        GPIO_ResetBits(GPIOB, GPIO_Pin_5); // PB5输出低电平, 点亮红灯
+        Delay_ms(500); // 延时500ms
+        GPIO_SetBits(GPIOB, GPIO_Pin_5); // PB5输出高电平, 熄灭红灯
+        Delay_ms(500); // 延时500ms
+    }
 
 }
